2c6c16ae259a2378c420fe1cb8c59c72a28b4df3
Valery Naumova <oliviia313@gmail.com>
2024-10-03 21:27:16 -0400

    added search and weather info update with api

--- src/script.js
+++ src/script.js
@@ -1,12 +1,26 @@
+function updateWeatherInfo(response) {
+  let tempElement = document.querySelector("#current-temp");
+  let temp = response.data.temperature.current;
+  let cityElement = document.querySelector("#current-city");
+  let city = `${
+    response.data.city.charAt(0).toUpperCase() + response.data.city.slice(1)
+  }`;
+  cityElement.innerHTML = city;
+  tempElement.innerHTML = Math.round(temp);
+}
+
+function searchCity(city) {
+  let key = "t9e2fb71e7f8f479occ3650b2b4f0a04";
+  let url = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${key}&units=metric`;
+  axios.get(url).then(updateWeatherInfo);
+}
+
 function handleSubmit(e) {
   e.preventDefault();
   let searchInput = document.querySelector(".search-input");
-  let cityElement = document.querySelector("#current-city");
-  let city = `${
-    searchInput.value.charAt(0).toUpperCase() + searchInput.value.slice(1)
-  }`;
-  cityElement.innerHTML = city;
+  searchCity(searchInput.value);
 }
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+searchCity("Paris");
